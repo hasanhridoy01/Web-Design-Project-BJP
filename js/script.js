4e507ab976e119f4892eb607c9f65228a9624c91
hridoy <hasanhridoy1367@gmail.com>
2020-09-21 00:46:03 +0600

    Bootstrap new feature done

--- js/script.js
+++ js/script.js
@@ -2,3 +2,8 @@ $('#firstName').keyup(function(){
 	var firstName = $('#firstName').val();
     $('#fname').text(firstName);
 });
+
+$('#nav li a').click(function(){
+    $('ul li a').removeClass('active');
+    $(this).addClass('active');
+});
